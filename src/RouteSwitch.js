09528401a042053b004ef222d5d5bfcc5c9e0a79
ChrisSyrnyk <christopher.syrnyk2@ucalgary.ca>
2023-12-20 19:11:35 -0700

    setup homepage

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -7,7 +7,7 @@ const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home-feed" element={<App />} />
+        <Route path="/" element={<App />} />
         <Route path="/blog-post/:postid" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
