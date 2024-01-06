3bbda526acccc9c357dc755f2575a82de6d85de5
ChrisSyrnyk <christopher.syrnyk2@ucalgary.ca>
2024-01-05 21:45:18 -0700

    reverted changes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import RouteSwitch from './RouteSwitch';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  /*
   <React.StrictMode>
     <RouteSwitch />
   </React.StrictMode>
+  */
+ <RouteSwitch />
 );
