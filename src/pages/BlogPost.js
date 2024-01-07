dcf55b6bc3f120e7eab627a3ad93eb7c796c81d9
ChrisSyrnyk <christopher.syrnyk2@ucalgary.ca>
2024-01-07 11:39:46 -0700

    added missing imports to BlogPost.js

--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -5,6 +5,8 @@ import footerstyles from '../styles/footer.css'
 import bodystyles from '../styles/body.css'
 import generalstyles from '../styles/general.css'
 import blogcardstyles from '../styles/blogcard.css'
+import blogstyles from '../styles/blog.css';
+import dropdownstyles from '../styles/dropdown.css';
 //import { Link } from "react-router-dom";
 
 import Header from '../components/Header';
